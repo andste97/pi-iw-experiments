edb4a43d894967f74664dbba1a48e48d78e75611
Andreas Stehle <andreas.stehle2@gmail.com>
2024-03-25 17:54:13 +0100

    fix max memory amount

--- scripts/hpc_submission_script.sh
+++ scripts/hpc_submission_script.sh
@@ -9,10 +9,10 @@
 #BSUB -n 8
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
-### -- specify that we need 4GB of memory per core/slot --
+### -- specify that we need 1GB of memory per core/slot --
 #BSUB -R "rusage[mem=1GB]"
-### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot --
-#BSUB -M 8GB
+### -- specify that we want the job to get killed if it exceeds 2 GB per core/slot --
+#BSUB -M 1GB
 ### -- set walltime limit: hh:mm --
 #BSUB -W 24:00
 ### -- set the email address --
