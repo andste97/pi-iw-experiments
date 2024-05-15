b101c639c6dffe30e6bd74106d11aabd54ac578e
Andreas Stehle <andreas.stehle2@gmail.com>
2024-05-15 10:35:55 +0200

    implement running entire test suite at once

--- piiw/online_planning_learning_v2_start_3_seeds.py
+++ piiw/online_planning_learning_v2_start_3_seeds.py
@@ -23,14 +23,15 @@ import pytorch_lightning as pl
 )
 def main(config):
     seeds = [0, 12345, 672354]
-    group_name = f'group_{config.train.env_id}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S.%f")}'
     processes = []
     process_info = {}
 
-    for seed in seeds:
-        process = multiprocessing.Process(target=start_run, args=(seed, config, group_name))
-        processes.append(process)
-        process_info[process.name] = {"seed": seed, "group": group_name, "start_time": time.time()}
+    for env_id in config.env_suite:
+        group_name = f'group_{config.train.env_id}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S.%f")}'
+        for seed in seeds:
+            process = multiprocessing.Process(target=start_run, args=(env_id, seed, config, group_name))
+            processes.append(process)
+            process_info[process.name] = {"env": env_id, "seed": seed, "group": group_name, "start_time": time.time()}
 
     for process in processes:
         process.start()
@@ -42,14 +43,16 @@ def main(config):
         end_time = time.time()
         runtime = end_time - process_info[process.name]["start_time"]
         exit_code = process.exitcode
+        env = process_info[process.name]["env"]
         seed = process_info[process.name]["seed"]
         group_name = process_info[process.name]["group"]
 
-        print(f"Process for seed {seed} in group '{group_name}' finished.")
+        print(f"Process for env: {env}, seed: {seed} in group: '{group_name}' finished.")
         print(f"Runtime: {runtime:.2f} seconds, Exit Code: {exit_code}")
 
-def start_run(seed, config, group_name):
+def start_run(env_id, seed, config, group_name):
     config.train.seed = seed
+    config.train.env_id = env_id
 
     # set seeds, numpy for planner, torch for policy
     seed_everything(config.train.seed)
