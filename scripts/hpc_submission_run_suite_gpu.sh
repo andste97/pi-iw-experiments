401110dd9c21207ef8809f021144c073ec49a04f
Andreas Stehle <andreas.stehle2@gmail.com>
2024-06-30 11:05:26 +0200

    updated submission scripts

--- scripts/hpc_submission_run_suite_gpu.sh
+++ scripts/hpc_submission_run_suite_gpu.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+##!/bin/sh
 
 ### General options
 ### -- specify queue --
-#BSUB -q hpc
+#BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J PI-IW-Atari
+#BSUB -J PI-IW-gpu
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 8
+#BSUB -n 13
+### -- Select the resources: 1 gpu in shared mode --
+#BSUB -gpu "num=1:mode=exclusive_process:mps=yes"
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
-### -- specify that we need 1GB of memory per core/slot --
-#BSUB -R "rusage[mem=1GB]"
-### -- specify that we want the job to get killed if it exceeds 2 GB per core/slot --
-#BSUB -M 1GB
+### -- specify that we need 20GB of memory --
+#BSUB -R "rusage[mem=2300MB]"
+### -- specify that we want the job to get killed if it exceeds 30GB --
+#BSUB -M 2500MB
 ### -- set walltime limit: hh:mm --
 #BSUB -W 24:00
 ### -- set the email address --
@@ -31,4 +33,6 @@
 # here follow the commands you want to execute with input.in as the input file
 # shellcheck disable=SC2039
 source ../venv/bin/activate
-python3 ../piiw/online_planning_learning_lightning.py --config-name config_atari_dynamic.yaml > output.out
+#wandb offline
+python3 ../piiw/online_planning_learning_run_entire_suite.py --config-name=config_atari_dynamic.yaml train.learning_rate=0.0005 train.total_interaction_budget=20000000 plan.softmax_temperature=0.5 plan.risk_averse=True plan.softmax_decay=True 'env_suite=["Pong-v4", "MsPacman-v4", "ChopperCommand-v4", "Breakout-v4"]' > output.out
+#wandb online
